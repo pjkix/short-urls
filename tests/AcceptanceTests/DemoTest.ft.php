f887a52dd06c8fd7ee186782062c578fe8232571
PJ Khalil <pj@pjkix.com>
2009-05-25 22:09:52 -0700

    browsers array

--- tests/AcceptanceTests/DemoTest.ft.php
+++ tests/AcceptanceTests/DemoTest.ft.php
@@ -10,9 +10,42 @@ require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 
 class DemoTest extends PHPUnit_Extensions_SeleniumTestCase
 {
+	
+	public static $browsers = array(
+	  array(
+		'name'	  => 'Firefox on Linux',
+		'browser' => '*firefox',
+		'host'	  => 'my.linux.box',
+		'port'	  => 4444,
+		'timeout' => 30000,
+	  ),
+	  array(
+		'name'	  => 'Safari on MacOS X',
+		'browser' => '*safari',
+		'host'	  => 'my.macosx.box',
+		'port'	  => 4444,
+		'timeout' => 30000,
+	  ),
+	  array(
+		'name'	  => 'Safari on Windows XP',
+		'browser' => '*custom C:\Program Files\Safari\Safari.exe -url',
+		'host'	  => 'my.windowsxp.box',
+		'port'	  => 4444,
+		'timeout' => 30000,
+	  ),
+	  array(
+		'name'	  => 'Internet Explorer on Windows XP',
+		'browser' => '*iexplore',
+		'host'	  => 'my.windowsxp.box',
+		'port'	  => 4444,
+		'timeout' => 30000,
+	  )
+	);
+	
 	function setUp()
 	{
-		$this->setBrowser("*firefox");
+		// $this->setBrowser("*firefox");
+		// $this->setBrowser("*safari"); // gets stuck ... maybe safari4?
 		$this->setBrowserUrl("http://localhost/");
 	}
 

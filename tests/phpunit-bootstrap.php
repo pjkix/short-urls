d44fb0c4416137fe3ba04bb27f05822147a2ef70
PJ Khalil <pj@pjkix.com>
2009-05-31 09:03:56 +0000

    turn off error repoting in phpunit bootstrap
    git-svn-id: http://svn.dev.pjkix.com/short-urls/trunk@409 5db93751-6b37-4446-8d8e-abc891bb5cf9

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // Set error reporting to highest level
-error_reporting( E_ALL | E_STRICT );
+// error_reporting( E_ALL | E_STRICT );
 
 // Allow time for the tests to run. I've scheduled a maximum of 3 minutes here.
 set_time_limit(180);
